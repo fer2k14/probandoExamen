71ff71d8cf9efb9c1a6dc141916560ff30eef95d
fer <fer@fer1>
2014-06-05 01:24:08 +0200

    Prueba2
    Signed-off-by: fer <fer@fer1>

---
+++ Debug1/src/PrincipalC.java
@@ -0,0 +1,14 @@
+
+public class PrincipalC {
+	int x = 2;
+	int y = 56;
+
+	public static void main(String[] args) {
+		
+	}
+	
+	public int suma(){
+		return this.x+this.y;
+	}
+
+}
---
+++ Debug1/src/Test/TestSuma.java
@@ -0,0 +1,20 @@
+package Test;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestSuma {
+	
+	
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
